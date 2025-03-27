7df12f0252074cc1f1f2d5041987eeca33a52a7e
Ngoni <ngonimapfumo@hotmail.com>
2025-03-28 01:14:44 +0200

    hello

--- Lists/ViewController.swift
+++ Lists/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+}
+    
+    extension ViewController: UITableViewDelegate {
+        
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            print("clicked\(indexPath)")
+        }
+        
+    }
+
+    extension ViewController: UITableViewDataSource {
+        
+        func tableView(_ tableView:UITableView,  numberOfRowsInSection section:Int) -> Int {
+            return 3
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            cell.textLabel?.text = "\(indexPath.row): Hello, World!"
+            
+            cell.detailTextLabel?.text = "Swift"
+            return cell
+        }
+        
     }
 
 
-}
+
 
