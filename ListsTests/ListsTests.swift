4feb2f95e1e875bed69bb59c44dc9d63c9f4c8a6
Ngoni <ngonimapfumo@hotmail.com>
2025-03-27 19:11:28 +0200

    Initial Commit

---
+++ ListsTests/ListsTests.swift
@@ -0,0 +1,17 @@
+//
+//  ListsTests.swift
+//  ListsTests
+//
+//  Created by Ngoni Mapfumo on 3/27/25.
+//
+
+import Testing
+@testable import Lists
+
+struct ListsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
